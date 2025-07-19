1a0281900d474e86d125995afe25fd9fbeaf1223
“Tecnarret” <Thornton.tc20@gmail.com>
2025-07-19 17:37:50 -0400

    Testing Git Connection

---
+++ Test_file_1.py
@@ -0,0 +1,10 @@
+# This is my second test file to upload to Github
+import pandas as pd
+
+
+data = {"line1": [1,2,3,4,5,6],
+        "line2": [6,7,8,9,10,11]}
+
+df = pd.DataFrame(data)
+
+print(df)
