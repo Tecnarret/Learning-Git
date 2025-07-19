ce34429a32fd92b98d8b50551fce1184350be644
“Tecnarret” <Thornton.tc20@gmail.com>
2025-07-19 17:48:26 -0400

    Test file update

--- Test_file_1.py
+++ Test_file_1.py
@@ -1,10 +1,24 @@
 # This is my second test file to upload to Github
 import pandas as pd
 
-
 data = {"line1": [1,2,3,4,5,6],
         "line2": [6,7,8,9,10,11]}
 
 df = pd.DataFrame(data)
 
-print(df)
+print(df) 
+
+
+
+
+# bar chart
+movies = ["Movie A", "Movie B", "Movie C"]
+ratings = [8.5, 7.2, 9.0]
+import matplotlib.pyplot as plt
+plt.bar(range(len(movies)), ratings)
+
+plt.title("My Movie Ratings")
+plt.xlabel("Movies")
+plt.ylabel("Ratings")
+
+plt.show()
